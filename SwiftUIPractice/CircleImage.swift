83279e11bb90acdb9cd48ff42b65a0e15b2f0756
shaunchuang <72535297+shaunchuang@users.noreply.github.com>
2024-03-31 21:05:14 +0800

    Complete First Tutorial

---
+++ SwiftUIPractice/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  SwiftUIPractice
+//
+//  Created by Shaun Chuang on 2024/3/31.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ SwiftUIPractice/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  SwiftUIPractice
+//
+//  Created by Shaun Chuang on 2024/3/31.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
